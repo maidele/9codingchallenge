8c2c2da13fee14b10d5cacda71ee0c9abc661cd9
Maidele Archelus <maidele@usf.edu>
2025-02-22 22:10:32 -0500

    Task 1 - Created Employee Class

--- cc_9.js
+++ cc_9.js
@@ -1,1 +1,25 @@
+//Task 1: Creating an Employee Class
 
+class Employee {
+    constructor(name, id, department, salary) {
+      this.name = name;
+      this.id = id;
+      this.department=department;
+      this.salary=salary;
+    }// Creating a class for employeee and their properties
+  
+    getDetails() {
+      return(`Employee:${this.name}, ID: ${this.id}, Department: S{this.department}, Salary:${this.salary}`);
+    }// Including a "getDetails()" method for employee's properties
+
+  calculateAnnualSalary() {
+    return this.salary*12;
+  }// Including a "calculateAnnualSalary()" method for an annual salary belonging to the employee
+};
+
+const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000);
+console.log(emp1.getDetails()); 
+// Expected output: "Employee: Alice Johnson, ID: 101, Department: Sales, Salary: $5000"
+
+console.log(emp1.calculateAnnualSalary()); 
+// Expected output: 60000
